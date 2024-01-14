baed7fed86ab185c2af585ae057ac143ea3c6885
bamgoesn <60645387+BamgoeSN@users.noreply.github.com>
2024-01-14 20:05:10 +0900

    Add string, format to prelude

--- basm-std/src/prelude.rs
+++ basm-std/src/prelude.rs
@@ -1,3 +1,3 @@
 pub use crate::{math::*, platform::io::*};
-pub use alloc::{collections::*, vec, vec::Vec};
+pub use alloc::{collections::*, format, string::*, vec, vec::Vec};
 pub use core::{cmp::*, iter, mem::*, num::*, ops::*};
