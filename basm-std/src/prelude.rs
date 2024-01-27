2a76c5e89a3911f23a34f2a4c7bde726f2d5cb7a
BamgoeSN <60645387+BamgoeSN@users.noreply.github.com>
2024-01-28 03:34:02 +0900

    Update prelude for function impls

--- basm-std/src/prelude.rs
+++ basm-std/src/prelude.rs
@@ -1,3 +1,4 @@
-pub use crate::{math::*, platform::io::*};
+pub use crate::{math::*, platform::{io::*, is_local_env}};
+pub use basm_macro::basm_export;
 pub use alloc::{collections::*, format, string::*, vec, vec::Vec};
 pub use core::{cmp::*, iter, mem::*, num::*, ops::*};
