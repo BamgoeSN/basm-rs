755fd5608ca86a809d73e7ba3b8949e77b39c96d
BamgoeSN <60645387+BamgoeSN@users.noreply.github.com>
2024-02-03 17:06:45 +0900

    Format solution.rs

--- basm/src/solution.rs
+++ basm/src/solution.rs
@@ -1,6 +1,6 @@
 use basm::prelude::*;
 
 pub fn main() {
-    let mut sc: Reader = Default::default();
-    let mut wr: Writer = Default::default();
+	let mut sc: Reader = Default::default();
+	let mut wr: Writer = Default::default();
 }
