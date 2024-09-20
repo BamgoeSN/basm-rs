d0bb6c00f0d960186214a07f601190a4d05a34ca
bamgoe <60645387+BamgoeSN@users.noreply.github.com>
2024-09-20 17:20:45 +0900

    Resolve conflict

--- basm-std/src/prelude.rs
+++ basm-std/src/prelude.rs
@@ -1,4 +1,3 @@
 pub use crate::{math::*, platform::{io::*, is_local_env}};
-pub use basm_macro::basm_export;
 pub use alloc::{collections::*, format, string::*, vec, vec::Vec};
 pub use core::{cmp::*, iter, mem::*, num::*, ops::*};
